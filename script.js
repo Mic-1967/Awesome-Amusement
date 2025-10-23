9284ac08b8ea800dcb55661729cf9e4df6399db2
Michael Williams <180468995+Mic-1967@users.noreply.github.com>
2025-10-23 01:34:45 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
 var rides = ["Tilt-a-Whirl", "Merry-Go-Round", "Bumper Cars", "Ferris Wheel", "Scrambler"];
 
-
 const getBirthday = () => {
   let dob = document.querySelector("#dob").value;
   let birthDate = new Date(dob);
-  console.log(birthDate.getFullYear);
 }
 const isRonald = () => {
   let username = document.querySelector("#name").value.trim();
-  if (username.toLowerCase() ==="ronald") {
+  if (username.toLowerCase() =="ronald") {
       alert("Thanks Ronald! Your purchase is 15% off!");
   } else {
-      alert("Thank you for your order. A representative will be in contact."); 
+         alert("Thank you for your order. A representative will be in contact."); 
   }
 }
 
@@ -20,18 +18,12 @@ const styleForm = () => {
   let formfields = document.querySelectorAll("input, select");
   for(let formfield of formfields){
       formfield.style.border = "3px solid yellow";
+    }
+           
+};
+document.addEventListener("DOMContentLoaded", () => {
+  styleForm();
+  document.querySelector("#submit").addEventListener("click", isRonald);
+});
 
-  }
-}
-const addOptions = () => {
-  for(let ride of rides){
-     const opt = document.createElement("option");
-     opt.value = ride;
-     opt.textContent = ride;
-      document.querySelector("#ride").appendChild(opt);      
-  }
- 
-}
-document.querySelector("#submit").addEventListener("click", isRonald);
-document.addEventListener("DOMContentLoaded", styleForm);
-document.addEventListener("DOMContentLoaded", addOptions);
+
