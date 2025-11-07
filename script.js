2c7e22f9700b4cb311dedb3de0771b494b95e114
Michael Williams <180468995+Mic-1967@users.noreply.github.com>
2025-11-06 23:51:57 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,19 @@
-fetch('sales.json')
+fetch('Ham King.JSON')
   .then(response => response.json())
-  .then(items => {
-    items.sales.forEach(item => { 
-      const section = document.getElementById(item.department);
-      if (section) {
-        const div = document.createElement('div');
-        div.classList.add('item-card');
-        div.innerHTML = `
-        <h3>${item.name}</h3>
-        <p>${item.description}</p>
-        <p><strong>Regular Price:</strong> $$
-        {item.regularPrice.toFixed(2)}</p>
-        <p><strong>Sale Price:</strong> $$
-        {item.salePrice.toFixed(2)}</p>
-        <img src="${item.image}" alt="${item.name}">width="120">` ;
-       
-       
-        section.appendChild(div);
-
-      }
+  .then(data => {
+      const container = document.getElementById('content)');
+      data.sales.forEach(item => {
+      const div = document.createElement;('div');
+      div.classList.add('item');
+      div.innerHTML = `
+      <h2>${item.name}</h2>
+      <p>${item.description}</p>
+      <p><strong>Regular Price:</strong> $${item.regular_Price.toFixed(2)}</p>
+      <p><strong>Sale Price:</strong> $${item.salePrice.toFixed(2)}</p>
+      <img src="${item.image}" alt="${item.name}" width="150">`;
+      container.appendChild(div);
     });
- })
-.catch(error => console.error('Error fetching sales data:', error));   
+  })
+  .catch(error => console.error('Error fetching sales data:', error));
+
+    
